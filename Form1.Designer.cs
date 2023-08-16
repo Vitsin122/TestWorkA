b40ab981140a6782534b3acbc9ec9ce374c5d1c2
Vitsin <warfire122@gmail.com>
2023-08-17 01:10:14 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TestWorkA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoogleMap = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // GoogleMap
+            // 
+            this.GoogleMap.Bearing = 0F;
+            this.GoogleMap.CanDragMap = true;
+            this.GoogleMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.GoogleMap.GrayScaleMode = false;
+            this.GoogleMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.GoogleMap.LevelsKeepInMemory = 5;
+            this.GoogleMap.Location = new System.Drawing.Point(12, 12);
+            this.GoogleMap.MarkersEnabled = true;
+            this.GoogleMap.MaxZoom = 2;
+            this.GoogleMap.MinZoom = 2;
+            this.GoogleMap.MouseWheelZoomEnabled = true;
+            this.GoogleMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.GoogleMap.Name = "GoogleMap";
+            this.GoogleMap.NegativeMode = false;
+            this.GoogleMap.PolygonsEnabled = true;
+            this.GoogleMap.RetryLoadTile = 0;
+            this.GoogleMap.RoutesEnabled = true;
+            this.GoogleMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.GoogleMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.GoogleMap.ShowTileGridLines = false;
+            this.GoogleMap.Size = new System.Drawing.Size(776, 426);
+            this.GoogleMap.TabIndex = 0;
+            this.GoogleMap.Zoom = 0D;
+            this.GoogleMap.Load += new System.EventHandler(this.GoogleMap_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GoogleMap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl GoogleMap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestWorkA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GoogleMap_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; //выбор подгрузки карты – онлайн или из ресурсов
+            GoogleMap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; //какой провайдер карт используется (в нашем случае гугл) 
+            GoogleMap.MinZoom = 2; //минимальный зум
+            GoogleMap.MaxZoom = 16; //максимальный зум
+            GoogleMap.Zoom = 4; // какой используется зум при открытии
+            GoogleMap.Position = new GMap.NET.PointLatLng(66.4169575018027, 94.25025752215694);// точка в центре карты при открытии (центр России)
+            GoogleMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; // как приближает (просто в центр карты или по положению мыши)
+            GoogleMap.CanDragMap = true; // перетаскивание карты мышью
+            GoogleMap.DragButton = MouseButtons.Left; // какой кнопкой осуществляется перетаскивание
+            GoogleMap.ShowCenter = false; //показывать или скрывать красный крестик в центре
+            GoogleMap.ShowTileGridLines = false; //показывать или скрывать тайлы
+        }
+    }
+}
